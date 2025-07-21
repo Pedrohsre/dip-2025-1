fd1cee9ede545b2e0772d995ec07294190aed291
Pedrohsre <phsr@ic.ufal.br>
2025-07-21 19:52:49 -0300

    first commit

--- tasks/task-01-generate-an-image/task-01-generate-an-image.py
+++ tasks/task-01-generate-an-image/task-01-generate-an-image.py
@@ -16,7 +16,8 @@ def generate_image(seed, width, height, mean, std):
         image (numpy.ndarray): The generated image.
     """
     ### START CODE HERE ###
-    ### TODO
+    np.random.seed(seed)
+    image = np.random.normal(loc=mean, scale=std, size=(height, width))
     ### END CODE HERE ###
     return image
 
@@ -39,6 +40,3 @@ if __name__ == "__main__":
 
     assert (np.abs(image - test) < 0.0001).all()
     print("Test passed!")
-
-
-
